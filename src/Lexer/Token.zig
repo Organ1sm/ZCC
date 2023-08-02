b0d8f41d65727eb19cfa62f5ce6980da019322e2
Organ1sm <ywwithpeople@gmail.com>
2023-08-02 17:25:42 +0800

    [Preprocessor]: support var args macro funtion.

--- src/Lexer/Token.zig
+++ src/Lexer/Token.zig
@@ -74,5 +74,6 @@ pub const Token = struct {
         .{ "error", .KeywordError },
         .{ "pragma", .KeywordPragma },
         .{ "line", .KeywordLine },
+        .{ "__VA_ARGS__", .KeywordVarArgs },
     });
 };
