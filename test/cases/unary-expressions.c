5c9e8e12d0a1d84c3582baddbd25e5d1e58295f4
Organ1sm <ywwithpeople@gmail.com>
2023-09-09 22:55:14 +0800

    [Parser]: add error for address of register varaiable

--- test/cases/unary-expressions.c
+++ test/cases/unary-expressions.c
@@ -20,6 +20,8 @@ void foo(void) {
     (void)!b;
     (void)!(void)1;
     (void);
+    register int d;
+    (void)&d;
 }
 
 
@@ -33,5 +35,5 @@ void foo(void) {
     "unary-expressions.c:17:11: error: invalid argument type 'double' to unary expression" \
     "unary-expressions.c:18:11: error: invalid argument type 'void' to unary expression" \
     "unary-expressions.c:21:11: error: invalid argument type 'void' to unary expression" \
-    "unary-expressions.c:22:11: error: expected expression"
-
+    "unary-expressions.c:22:11: error: expected expression" \
+    "unary-expressions.c:24:11: error: address off register variable requested"
