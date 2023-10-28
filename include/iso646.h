d02e309c3675c33d1630efe25d1053c0c82aff32
Organ1sm <ywwithpeople@gmail.com>
2023-10-28 23:19:41 +0800

    [Compilation]: add header for iso646.h and stdnoreturn.h

---
+++ include/iso646.h
@@ -0,0 +1,16 @@
+/* <iso646.h> for the ZCC C compiler */
+
+#pragma once
+
+#define and    &&
+#define and_eq &=
+#define bitand &
+#define bitor  |
+#define compl  ~
+#define not    !
+#define not_eq !=
+#define or     ||
+#define or_eq  |=
+#define xor    ^
+#define xor_eq ^=
+
