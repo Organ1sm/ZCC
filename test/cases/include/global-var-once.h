9fd92d85fab58b53b8530a0bc2c07a916bbb0b0a
Organ1sm <ywwithpeople@gmail.com>
2023-11-16 20:34:26 +0800

    [Preprocessor]: add tests for #pragma once

---
+++ test/cases/include/global-var-once.h
@@ -0,0 +1,3 @@
+#pragma once
+int once = 10;
+
---
+++ test/cases/pragma-once.h
@@ -0,0 +1,9 @@
+#include "include/global-var.h"
+#include "include/global-var.h"
+
+#include "include/global-var-once.h"
+#include "include/global-var-once.h"
+
+#define EXPECTED_ERRORS "global-var.h:1:5: error: redefinition of 'multiple'" \
+	"global-var.h:1:5: note: previous definition is here"
+
