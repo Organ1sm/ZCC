3dd463064e095dc245266f0ce9e6b608c8d032d6
Organ1sm <ywwithpeople@gmail.com>
2024-01-07 13:10:40 +0800

    [Diagnostics/GCC/Pragma]: implement GCC diagnostics push/pop

---
+++ test/cases/adjust-diagnostic-levels.c
@@ -0,0 +1,23 @@
+#define FOO 1
+#define FOO 2
+
+#pragma GCC diagnostic error "-Wmacro-redefined"
+#define BAR 1
+#define BAR 2
+
+#pragma GCC diagnostic push
+
+#pragma GCC diagnostic ignored "-Wmacro-redefined"
+#define BAZ 1
+#define BAZ 2
+
+#pragma GCC diagnostic pop
+#pragma GCC diagnostic pop
+
+#define QUX 1
+#define QUX 2
+
+
+#define EXPECTED_ERRORS "adjust-diagnostic-levels.c:2:9: warning: 'FOO' macro redefined" \
+	"adjust-diagnostic-levels.c:6:9: error: 'BAR' macro redefined" \
+	"adjust-diagnostic-levels.c:18:9: warning: 'QUX' macro redefined [-Wmacro-redefined]" \
