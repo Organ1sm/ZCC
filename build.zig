bdd11de34ea4b7d179c3de9801d0038bc9ac1fdc
Organ1sm <ywwithpeople@gmail.com>
2024-01-12 19:12:34 +0800

    [Test]: add fuzz test

--- build.zig
+++ build.zig
@@ -1,9 +1,57 @@
 const std = @import("std");
+const ZigLibDirStep = @import("build/ZigLibDir.zig");
+
+fn addFuzzStep(b: *std.Build, target: std.Build.ResolvedTarget, aflClangLTOPath: []const u8, zccModule: *std.Build.Module) !void {
+    const fuzzStep = b.step("fuzz", "Build executable for fuzz testing.");
+    var fuzzTarget = target;
+    fuzzTarget.result.ofmt = .c;
+
+    const libDirStep = try ZigLibDirStep.create(b);
+    const compiler_rt = b.createModule(.{
+        .root_source_file = libDirStep.getCompilerRTPath(),
+    });
+
+    const fuzz_lib = b.addStaticLibrary(.{
+        .name = "fuzz-lib",
+        .root_source_file = .{ .path = "test/fuzz/fuzz_lib.zig" },
+        .optimize = .Debug,
+        .target = fuzzTarget,
+        .single_threaded = true,
+    });
+
+    fuzz_lib.root_module.addImport("zcc", zccModule);
+    fuzz_lib.root_module.addImport("compiler_rt", compiler_rt);
+
+    // We want `afl-clang-lto -o path/to/output test/fuzz/main.c path/to/library`
+    const fuzz_compile = b.addSystemCommand(&.{
+        aflClangLTOPath,
+        "-Wno-incompatible-pointer-types",
+        "-nostdinc",
+        "-isystem",
+    });
+    fuzz_compile.addArgs(&.{
+        "-isystem",
+        "/usr/include",
+        "-isystem",
+        "/usr/local/include",
+        "-std=c99",
+    });
+    fuzz_compile.addFileArg(.{ .path = "test/fuzz/main.c" });
+    fuzz_compile.addArg("-I");
+    fuzz_compile.addDirectoryArg(libDirStep.getLibPath());
+    fuzz_compile.addArg("-o");
+    const fuzz_exe = fuzz_compile.addOutputFileArg("zccfuzz");
+    const fuzz_install = b.addInstallBinFile(fuzz_exe, "zccfuzz");
+
+    // Add the path to the library file to afl-clang-lto's args
+    fuzz_compile.addArtifactArg(fuzz_lib);
+    fuzzStep.dependOn(&fuzz_install.step);
+}
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -93,4 +141,7 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(integration_tests);
     test_step.dependOn(&integration_test_runner.step);
+
+    const afl_clang_lto_path = b.option([]const u8, "afl-clang-lto-path", "Path to afl-clang-lto") orelse "afl-clang-lto";
+    try addFuzzStep(b, target, afl_clang_lto_path, zccModule);
 }
---
+++ test/fuzz/fuzz_lib.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+const zcc = @import("zcc");
+const mem = std.mem;
+const Allocator = mem.Allocator;
+const process = std.process;
+const Codegen = zcc.CodeGen;
+const Compilation = zcc.Compilation;
+const Source = zcc.Source;
+const Preprocessor = zcc.Preprocessor;
+const Parser = zcc.Parser;
+
+var fixed_buffer_mem: [10 * 1024 * 1024]u8 = undefined;
+
+export fn compile_c_buf(buf: [*]const u8, len: c_int) void {
+    compileSlice(buf[0..@as(usize, @intCast(len))]) catch unreachable;
+}
+
+fn compileSlice(buf: []const u8) !void {
+    var fixed_allocator = std.heap.FixedBufferAllocator.init(fixed_buffer_mem[0..]);
+    const allocator = fixed_allocator.allocator();
+
+    var comp = Compilation.init(allocator);
+    defer comp.deinit();
+
+    try comp.addDefaultPragmaHandlers();
+    try comp.defineSystemIncludes();
+
+    const builtin = try comp.generateBuiltinMacros();
+    var userSource = Source{
+        .id = @as(Source.ID, @enumFromInt(comp.sources.count() + 2)),
+        .path = "<STDIN>",
+        .buffer = buf,
+    };
+    userSource.checkUtf8();
+    try comp.sources.put(userSource.path, userSource);
+
+    processSource(&comp, builtin, userSource) catch |e| switch (e) {
+        error.FatalError => {},
+        else => |err| return err,
+    };
+    _ = comp.sources.swapRemove(userSource.path);
+}
+
+fn processSource(comp: *Compilation, builtin: Source, userSource: Source) !void {
+    var pp = Preprocessor.init(comp);
+    defer pp.deinit();
+    try pp.addBuiltinMacros();
+
+    _ = try pp.preprocess(builtin);
+    const eof = try pp.preprocess(userSource);
+    try pp.tokens.append(pp.compilation.gpa, eof);
+
+    var tree = try Parser.parse(&pp);
+    defer tree.deinit();
+
+    try tree.dump(std.io.null_writer);
+}
