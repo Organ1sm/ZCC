61dca697ee219e4df9b74d7aa6edc35721eccc34
Organ1sm <ywwithpeople@gmail.com>
2024-09-28 16:47:16 +0800

    [Interner]: begin moving values to interner

--- src/CodeGen/Interner.zig
+++ src/CodeGen/Interner.zig
@@ -2,60 +2,85 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
 const Value = @import("../AST/Value.zig");
+const BigIntConst = std.math.big.int.Const;
+const BigIntMutable = std.math.big.int.Mutable;
+const Hash = std.hash.Wyhash;
 
 const Interner = @This();
 
-map: std.ArrayHashMapUnmanaged(Key, void, KeyContext, false) = .{},
+map: std.AutoArrayHashMapUnmanaged(void, void) = .{},
+items: std.MultiArrayList(struct {
+    tag: Tag,
+    data: u32,
+}) = .{},
+extra: std.ArrayListUnmanaged(u32) = .{},
+strings: std.ArrayListUnmanaged(u8) = .{},
 
-const KeyContext = struct {
-    pub fn eql(_: @This(), a: Key, b: Key, _: usize) bool {
-        return b.eql(a);
+const KeyAdapter = struct {
+    interner: *const Interner,
+
+    pub fn eql(adapter: KeyAdapter, a: Key, b_void: void, b_map_index: usize) bool {
+        _ = b_void;
+        return adapter.interner.get(@as(Ref, @enumFromInt(b_map_index))).eql(a);
     }
 
-    pub fn hash(_: @This(), a: Key) u32 {
+    pub fn hash(adapter: KeyAdapter, a: Key) u32 {
+        _ = adapter;
         return a.hash();
     }
 };
 
 pub const Key = union(enum) {
-    int: u16,
-    float: u16,
-    ptr,
-    noreturn,
-    void,
-    func,
-    array: struct {
+    intTy: u16,
+    floatTy: u16,
+    ptrTy,
+    noreturnTy,
+    voidTy,
+    funcTy,
+    arrayTy: struct {
         len: u64,
         child: Ref,
     },
-    vector: struct {
+    vectorTy: struct {
         len: u32,
         child: Ref,
     },
-    value: Value,
-
+    /// may not be zero
+    null,
+    int: union(enum) {
+        u64: u64,
+        i64: i64,
+        bigInt: BigIntConst,
+    },
+    float: Float,
+    bytes: []const u8,
     record: struct {
         /// Pointer to user data, value used for hash and equality check.
         userPtr: *anyopaque,
         elements: []const Ref,
     },
 
+    pub const Float = union(enum) {
+        f16: f16,
+        f32: f32,
+        f64: f64,
+        f80: f80,
+        f128: f128,
+    };
+
     pub fn hash(key: Key) u32 {
-        var hasher = std.hash.Wyhash.init(0);
+        var hasher = Hash.init(0);
+        const tag = std.meta.activeTag(key);
+        std.hash.autoHash(&hasher, tag);
         switch (key) {
-            .value => |val| {
-                std.hash.autoHash(&hasher, val.tag);
-                switch (val.tag) {
-                    .unavailable => unreachable,
-                    .nullptrTy => std.hash.autoHash(&hasher, @as(u64, 0)),
-                    .int => std.hash.autoHash(&hasher, val.data.int),
-                    .float => std.hash.autoHash(&hasher, @as(u64, @bitCast(val.data.float))),
-                    .bytes => std.hash.autoHashStrat(&hasher, val.data.bytes, .Shallow),
-                }
+            .bytes => |bytes| {
+                hasher.update(bytes);
             },
             .record => |info| {
                 std.hash.autoHash(&hasher, @intFromPtr(info.userPtr));
             },
+            .float => @panic("TODO"),
+            .int => @panic("TODO"),
             inline else => |a_info| {
                 std.hash.autoHash(&hasher, a_info);
             },
@@ -69,23 +94,13 @@ pub const Key = union(enum) {
         const rhsTag: KeyTag = rhs;
         if (lhsTag != rhsTag) return false;
         switch (lhs) {
-            .value => |lhsInfo| {
-                const rhsInfo = rhs.value;
-                if (lhsInfo.tag != rhsInfo.tag) return false;
-                switch (lhsInfo.tag) {
-                    .unavailable => unreachable,
-                    .nullptrTy => return true,
-                    .int => return lhsInfo.data.int == rhsInfo.data.int,
-                    .float => return lhsInfo.data.float == rhsInfo.data.float,
-                    .bytes => return (lhsInfo.data.bytes.start == rhsInfo.data.bytes.start) and
-                        (lhsInfo.data.bytes.end == rhsInfo.data.bytes.end),
-                }
-            },
-
             .record => |lhsInfo| {
                 return lhsInfo.userPtr == rhs.record.userPtr;
             },
-
+            .bytes => |lhsBytes| {
+                const rhsBytes = rhs.bytes;
+                return std.mem.eql(u8, lhsBytes, rhsBytes);
+            },
             inline else => |lhsInfo, tag| {
                 const rhsInfo = @field(rhs, @tagName(tag));
                 return std.meta.eql(lhsInfo, rhsInfo);
@@ -95,7 +110,7 @@ pub const Key = union(enum) {
 
     fn toRef(key: Key) ?Ref {
         switch (key) {
-            .int => |bits| switch (bits) {
+            .intTy => |bits| switch (bits) {
                 1 => return .i1,
                 8 => return .i8,
                 16 => return .i16,
@@ -104,7 +119,7 @@ pub const Key = union(enum) {
                 128 => return .i128,
                 else => {},
             },
-            .float => |bits| switch (bits) {
+            .floatTy => |bits| switch (bits) {
                 16 => return .f16,
                 32 => return .f32,
                 64 => return .f64,
@@ -112,10 +127,19 @@ pub const Key = union(enum) {
                 128 => return .f128,
                 else => unreachable,
             },
-            .ptr => return .ptr,
-            .func => return .func,
-            .noreturn => return .noreturn,
-            .void => return .void,
+            .ptrTy => return .ptr,
+            .funcTy => return .func,
+            .noreturnTy => return .noreturn,
+            .voidTy => return .void,
+            .int => |repr| switch (repr) {
+                inline .u64, .i64 => |u| switch (u) {
+                    0 => return .zero,
+                    1 => return .one,
+                    else => {},
+                },
+                .bigInt => |data| if (data.eqlZero()) return .zero,
+            },
+            .null => return .null,
             else => {},
         }
         return null;
@@ -123,34 +147,200 @@ pub const Key = union(enum) {
 };
 
 pub const Ref = enum(u32) {
-    const max = std.math.maxInt(u32);
-
-    ptr = max - 0,
-    noreturn = max - 1,
-    void = max - 2,
-    i1 = max - 3,
-    i8 = max - 4,
-    i16 = max - 5,
-    i32 = max - 6,
-    i64 = max - 7,
-    i128 = max - 8,
-    f16 = max - 9,
-    f32 = max - 10,
-    f64 = max - 11,
-    f80 = max - 12,
-    f128 = max - 13,
-    func = max - 14,
+    ptr,
+    noreturn,
+    void,
+    i1,
+    i8,
+    i16,
+    i32,
+    i64,
+    i128,
+    f16,
+    f32,
+    f64,
+    f80,
+    f128,
+    func,
+    zero,
+    one,
+    null,
     _,
+};
+
+pub const Tag = enum(u8) {
+    /// `data` is `u16`
+    intTy,
+    /// `data` is `u16`
+    floatTy,
+    /// `data` is index to `Array`
+    arrayTy,
+    /// `data` is index to `Vector`
+    vectorTy,
+    /// `data` is `u32`
+    u32,
+    /// `data` is `i32`
+    i32,
+    /// `data` is `Int`
+    intPositive,
+    /// `data` is `Int`
+    intNegative,
+    /// `data` is `f16`
+    f16,
+    /// `data` is `f32`
+    f32,
+    /// `data` is `F64`
+    f64,
+    /// `data` is `F80`
+    f80,
+    /// `data` is `F128`
+    f128,
+    /// `data` is `Bytes`
+    bytes,
+    /// `data` is `Record`
+    record,
+
+    pub const Array = struct {
+        len0: u32,
+        len1: u32,
+        child: Ref,
+
+        pub fn getLen(a: Array) u64 {
+            return (PackedU64{
+                .a = a.len0,
+                .b = a.len1,
+            }).get();
+        }
+    };
+
+    pub const Vector = struct {
+        len: u32,
+        child: Ref,
+    };
+
+    pub const Int = struct {
+        limbs_index: u32,
+        limbs_len: u32,
+
+        /// Big enough to fit any non-BigInt value
+        pub const BigIntSpace = struct {
+            /// The +1 is headroom so that operations such as incrementing once
+            /// or decrementing once are possible without using an allocator.
+            limbs: [(@sizeOf(u64) / @sizeOf(std.math.big.Limb)) + 1]std.math.big.Limb,
+        };
+    };
+
+    pub const F64 = struct {
+        piece0: u32,
+        piece1: u32,
+
+        pub fn get(self: F64) f64 {
+            const int_bits = @as(u64, self.piece0) | (@as(u64, self.piece1) << 32);
+            return @bitCast(int_bits);
+        }
+
+        fn pack(val: f64) F64 {
+            const bits = @as(u64, @bitCast(val));
+            return .{
+                .piece0 = @as(u32, @truncate(bits)),
+                .piece1 = @as(u32, @truncate(bits >> 32)),
+            };
+        }
+    };
+
+    pub const F80 = struct {
+        piece0: u32,
+        piece1: u32,
+        piece2: u32, // u16 part, top bits
+
+        pub fn get(self: F80) f80 {
+            const int_bits = @as(u80, self.piece0) |
+                (@as(u80, self.piece1) << 32) |
+                (@as(u80, self.piece2) << 64);
+            return @bitCast(int_bits);
+        }
+
+        fn pack(val: f80) F80 {
+            const bits = @as(u80, @bitCast(val));
+            return .{
+                .piece0 = @as(u32, @truncate(bits)),
+                .piece1 = @as(u32, @truncate(bits >> 32)),
+                .piece2 = @as(u16, @truncate(bits >> 64)),
+            };
+        }
+    };
+
+    pub const F128 = struct {
+        piece0: u32,
+        piece1: u32,
+        piece2: u32,
+        piece3: u32,
+
+        pub fn get(self: F128) f128 {
+            const int_bits = @as(u128, self.piece0) |
+                (@as(u128, self.piece1) << 32) |
+                (@as(u128, self.piece2) << 64) |
+                (@as(u128, self.piece3) << 96);
+            return @bitCast(int_bits);
+        }
+
+        fn pack(val: f128) F128 {
+            const bits = @as(u128, @bitCast(val));
+            return .{
+                .piece0 = @as(u32, @truncate(bits)),
+                .piece1 = @as(u32, @truncate(bits >> 32)),
+                .piece2 = @as(u32, @truncate(bits >> 64)),
+                .piece3 = @as(u32, @truncate(bits >> 96)),
+            };
+        }
+    };
+
+    pub const Bytes = struct {
+        stringsIndex: u32,
+        len: u32,
+    };
+
+    pub const Record = struct {
+        ptr0: u32,
+        ptr1: u32,
+        elements_len: u32,
+        // trailing
+        // [elements_len]Ref
+
+        pub fn getPtr(r: Record) *anyopaque {
+            return @ptrFromInt((PackedU64{
+                .a = r.ptr0,
+                .b = r.ptr1,
+            }).get());
+        }
+    };
+};
+
+pub const PackedU64 = packed struct(u64) {
+    a: u32,
+    b: u32,
+
+    pub fn get(x: PackedU64) u64 {
+        return @bitCast(x);
+    }
+
+    pub fn init(x: u64) PackedU64 {
+        return @bitCast(x);
+    }
 };
 
 pub fn deinit(self: *Interner, gpa: Allocator) void {
     self.map.deinit(gpa);
+    self.items.deinit(gpa);
+    self.extra.deinit(gpa);
+    self.strings.deinit(gpa);
 }
 
 pub fn put(self: *Interner, gpa: Allocator, key: Key) !Ref {
     if (key.toRef()) |some|
         return some;
-    const gop = try self.map.getOrPut(gpa, key);
+    const adapter = KeyAdapter{ .interner = self };
+    const gop = try self.map.getOrPutAdapted(gpa, key, adapter);
     return @enumFromInt(gop.index);
 }
 
