42af06ddb3be05a0c389810935c655eb36501e5d
Organ1sm <ywwithpeople@gmail.com>
2025-04-18 15:29:03 +0800

    [Value]: add complex division support

--- src/zinc/AnnexG.zig
+++ src/zinc/AnnexG.zig
@@ -5,8 +5,12 @@ const std = @import("std");
 const copysign = std.math.copysign;
 const inf = std.math.inf;
 const nan = std.math.nan;
+const ilogb = std.math.ilogb;
 const isInf = std.math.isInf;
 const isNan = std.math.isNan;
+const isFinite = std.math.isFinite;
+const isPositiveZero = std.math.isPositiveZero;
+const scalbn = std.math.scalbn;
 
 /// computes floating point z*w where a_param, b_param are real, imaginary parts of z and c_param, d_param are real, imaginary parts of w
 pub fn complexFloatMul(comptime T: type, aParam: T, bParam: T, cParam: T, dParam: T) [2]T {
@@ -56,9 +60,52 @@ pub fn complexFloatMul(comptime T: type, aParam: T, bParam: T, cParam: T, dParam
     return .{ x, y };
 }
 
+/// computes floating point z / w where a_param, b_param are real, imaginary parts of z and c_param, d_param are real, imaginary parts of w
+pub fn complexFloatDiv(comptime T: type, aParam: T, bParam: T, cParam: T, dParam: T) [2]T {
+    var a = aParam;
+    var b = bParam;
+    var c = cParam;
+    var d = dParam;
+    var denomLogb: i32 = 0;
+
+    const maxCD = @max(@abs(c), @abs(d));
+    if (isFinite(maxCD)) {
+        denomLogb = ilogb(maxCD);
+        c = scalbn(c, -denomLogb);
+        d = scalbn(d, -denomLogb);
+    }
+    const denom = c * c + d * d;
+    var x = scalbn((a * c + b * d) / denom, -denomLogb);
+    var y = scalbn((b * c - a * d) / denom, -denomLogb);
+    if (isNan(x) and isNan(y)) {
+        if (isPositiveZero(denom) and (!isNan(a) or !isNan(b))) {
+            x = copysign(inf(T), c) * a;
+            y = copysign(inf(T), c) * b;
+        } else if ((isInf(a) or isInf(b)) and isFinite(c) and isFinite(d)) {
+            a = copysign(if (isInf(a)) @as(T, 1.0) else @as(T, 0.0), a);
+            b = copysign(if (isInf(b)) @as(T, 1.0) else @as(T, 0.0), b);
+            x = inf(T) * (a * c + b * d);
+            y = inf(T) * (b * c - a * d);
+        } else if (isInf(maxCD) and isFinite(a) and isFinite(b)) {
+            c = copysign(if (isInf(c)) @as(T, 1.0) else @as(T, 0.0), c);
+            d = copysign(if (isInf(d)) @as(T, 1.0) else @as(T, 0.0), d);
+            x = 0.0 * (a * c + b * d);
+            y = 0.0 * (b * c - a * d);
+        }
+    }
+    return .{ x, y };
+}
+
 test complexFloatMul {
     // Naive algorithm would produce NaN + NaNi instead of inf + NaNi
-    const result = complexFloatMul(f64, inf(f64), nan(f64), 2, 0);
+    const result = complexFloatMul(f64, inf(f64), std.math.nan(f64), 2, 0);
+    try std.testing.expect(isInf(result[0]));
+    try std.testing.expect(isNan(result[1]));
+}
+
+test complexFloatDiv {
+    // Naive algorithm would produce NaN + NaNi instead of inf + NaNi
+    const result = complexFloatDiv(f64, inf(f64), std.math.nan(f64), 2, 0);
     try std.testing.expect(isInf(result[0]));
     try std.testing.expect(isNan(result[1]));
 }
